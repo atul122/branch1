e723658034b76c634db3e3c21ddf35054e570cc7
atulrathodc <atul.rathod@bonzai.co>
2018-11-19 15:16:49 +0530

    fg

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit} from '@angular/core';
+import * as d3 from "d3";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit  {
+  title = 'rr';
+
+  ngOnChanges() {
+    console.log(`ngOnChanges - data is `);
+  }
+
+  ngOnInit() {
+    console.log(`ngOnInit  - data is `,d3);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck")
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked1");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+}
